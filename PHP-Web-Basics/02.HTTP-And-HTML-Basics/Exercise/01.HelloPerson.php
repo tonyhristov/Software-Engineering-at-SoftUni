e33c418e79a65c5691ef7a5cd2ce4eb9da630db7
Antonio <47687247+tonyhristov@users.noreply.github.com>
2019-05-23 21:59:46 +0300

    Exercise

---
+++ PHP-Web-Basics/02.HTTP-And-HTML-Basics/Exercise/01.HelloPerson.php
@@ -0,0 +1,12 @@
+<form>
+    Name: <input type="text" name="person"/>
+    <input type="submit"/>
+</form>
+
+<?php
+
+if (isset($_GET["person"])) {
+    $name = htmlspecialchars($_GET["person"]);
+
+    echo "Hello, $name!";
+}
