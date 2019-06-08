961c25f943e06c0684dd8b741a0ca4d2d8d03bce
Antonio <47687247+tonyhristov@users.noreply.github.com>
2019-06-09 00:17:06 +0300

    Exercise

--- PHP-Web-Basics/06.Defining-Classes/18.OldestFamilyMember.php
+++ PHP-Web-Basics/06.Defining-Classes/18.OldestFamilyMember.php
@@ -16,9 +16,59 @@ class Person
         $this->age = $age;
     }
 
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
 }
 
 class Family
 {
-    
+    private $members;
+
+    /**
+     * @var Person
+     */
+    private $OldestMember;
+
+    public function __construct()
+    {
+        $this->members = [];
+        $this->OldestMember = null;
+    }
+
+    public function addMember(Person $person): void
+    {
+        if ($this->OldestMember === null || $this->OldestMember->getAge() < $person->getAge()) {
+            $this->OldestMember = $person;
+        }
+        $this->members[] = $person;
+    }
+
+    public function getOldestMember(): Person
+    {
+        return $this->OldestMember;
+    }
 }
+
+$input = readline();
+$family = new Family();
+
+for ($i = 0; $i < $input; $i++) {
+    list($name, $age) = explode(" ", readline());
+    $person = new Person($name, $age);
+    $family->addMember($person);
+}
+
+echo $family->getOldestMember()->getName() . " " . $family->getOldestMember()->getAge();
