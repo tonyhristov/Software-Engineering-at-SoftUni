640ce9ee7cc968a14aaaca53fa4380743416cbf3
Antonio <47687247+tonyhristov@users.noreply.github.com>
2019-06-08 23:33:34 +0300

    Exercise (Not Ready)

---
+++ PHP-Web-Basics/06.Defining-Classes/18.OldestFamilyMember.php
@@ -0,0 +1,24 @@
+<?php
+
+class Person
+{
+    private $name;
+    private $age;
+
+    /**
+     * Person constructor.
+     * @param $name
+     * @param $age
+     */
+    public function __construct($name, $age)
+    {
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+}
+
+class Family
+{
+    
+}
