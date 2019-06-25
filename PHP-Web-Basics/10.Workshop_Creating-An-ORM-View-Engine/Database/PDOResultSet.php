f661688eb3fd3ac58bebd7a65283a3bcffb4a5ea
Antonio <47687247+tonyhristov@users.noreply.github.com>
2019-06-25 21:20:03 +0300

    Exercise

---
+++ PHP-Web-Basics/10.Workshop_Creating-An-ORM-View-Engine/Database/PDOResultSet.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace Database;
+
+
+class PDOResultSet implements ResultSetInterface
+{
+
+    private $pdoStatement;
+
+
+    public function __construct(\PDOStatement $PDOStatement)
+    {
+        $this->pdoStatement = $PDOStatement;
+    }
+
+
+    /**
+     * @param $className
+     * @return \Generator
+     */
+    public function fetch($className): \Generator
+    {
+        while ($row = $this->pdoStatement->fetchObject($className)) {
+            yield $row;
+        }
+    }
+}
