926b1074e442a4987e0145452c76f0da798dc0bc
Antonio <47687247+tonyhristov@users.noreply.github.com>
2019-07-03 21:15:27 +0300

    Exercise

---
+++ PHP-Web-Basics/11.Exam-Preparation/App/Data/BookDTO.php
@@ -0,0 +1,177 @@
+<?php
+
+
+namespace App\Data;
+
+
+class BookDTO
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @var string
+     */
+    private $author;
+
+    /**
+     * @var string
+     */
+    private $description;
+
+    /**
+     * @var string
+     */
+    private $imageUrl;
+
+    /**
+     * @var GenreDTO
+     */
+    private $genre;
+
+    /**
+     * @var UserDTO
+     */
+    private $user;
+
+    /**
+     * @var string
+     */
+    private $addedOn;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAuthor(): string
+    {
+        return $this->author;
+    }
+
+    /**
+     * @param string $author
+     */
+    public function setAuthor(string $author): void
+    {
+        $this->author = $author;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return string
+     */
+    public function getImageUrl(): string
+    {
+        return $this->imageUrl;
+    }
+
+    /**
+     * @param string $imageUrl
+     */
+    public function setImageUrl(string $imageUrl): void
+    {
+        $this->imageUrl = $imageUrl;
+    }
+
+    /**
+     * @return GenreDTO
+     */
+    public function getGenre(): GenreDTO
+    {
+        return $this->genre;
+    }
+
+    /**
+     * @param GenreDTO $genre
+     */
+    public function setGenre(GenreDTO $genre): void
+    {
+        $this->genre = $genre;
+    }
+
+    /**
+     * @return UserDTO
+     */
+    public function getUser(): UserDTO
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param UserDTO $user
+     */
+    public function setUser(UserDTO $user): void
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAddedOn(): string
+    {
+        return $this->addedOn;
+    }
+
+    /**
+     * @param string $addedOn
+     */
+    public function setAddedOn(string $addedOn): void
+    {
+        $this->addedOn = $addedOn;
+    }
+
+}
