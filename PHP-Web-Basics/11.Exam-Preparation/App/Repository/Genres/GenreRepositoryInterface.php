00cc080329a40a2e2c2552cfd2b213de9dcd8e3c
Antonio <47687247+tonyhristov@users.noreply.github.com>
2019-07-03 21:16:11 +0300

    Exercise

---
+++ PHP-Web-Basics/11.Exam-Preparation/App/Repository/Genres/GenreRepositoryInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\Repository\Genres;
+
+
+use App\Data\GenreDTO;
+
+interface GenreRepositoryInterface
+{
+    /**
+     * @return \Generator | GenreDTO[]
+     */
+    public function findAll(): \Generator;
+
+    public function findOneById(int $id): GenreDTO;
+}
---
+++ PHP-Web-Basics/11.Exam-Preparation/App/Repository/UserRepository.php
@@ -0,0 +1,110 @@
+<?php
+
+
+namespace App\Repository;
+
+
+use App\Data\UserDTO;
+use Database\DatabaseInterface;
+
+class UserRepository extends DatabaseAbstract implements UserRepositoryInterface
+{
+
+    public function insert(UserDTO $userDTO): bool
+    {
+        $this->db->query(
+            "INSERT INTO users(username, password, full_name, born_on)
+                   VALUES (?, ?, ?, ?)
+             "
+        )->execute(
+            [
+                $userDTO->getUsername(),
+                $userDTO->getPassword(),
+                $userDTO->getFullName(),
+                $userDTO->getBornOn(),
+            ]);
+        return true;
+    }
+
+
+    public function update(int $id, UserDTO $userDTO): bool
+    {
+        $this->db->query(
+            "
+                    UPDATE users
+                    SET
+                        username = ?,
+                        password = ?,
+                        full_name = ?,
+                        born_on = ?
+                    WHERE id = ?
+                  "
+        )->execute(
+            [
+                $userDTO->getUsername(),
+                $userDTO->getPassword(),
+                $userDTO->getFullName(),
+                $userDTO->getBornOn(),
+                $id
+            ]);
+        return true;
+    }
+
+    public function delete(int $id): bool
+    {
+        $this->db->query("DELETE FROM users WHERE id = ?")
+            ->execute([$id]);
+        return true;
+    }
+
+    public function findOneByUsername(string $username): ?UserDTO
+    {
+        return $this->db->query(
+            "
+                    SELECT id, 
+                        username, 
+                        password, 
+                        full_name AS firstName, 
+                        born_on as bornOn
+                    FROM users
+                    WHERE username = ?
+                  "
+        )->execute([$username])
+            ->fetch(UserDTO::class)
+            ->current();
+    }
+
+    public function findOneById(int $id): ?UserDTO
+    {
+        return $this->db->query(
+            "SELECT id, 
+                        username, 
+                        password, 
+                        full_name AS fullName,
+                        born_on as bornOn
+                   FROM users
+                   WHERE id = ?
+                  "
+        )->execute([$id])
+            ->fetch(UserDTO::class)
+            ->current();
+    }
+
+    /**
+     * @return \Generator | UserDTO[]
+     */
+    public function findAll(): \Generator
+    {
+        return $this->db->query(
+            "
+                    SELECT id, 
+                        username, 
+                        password, 
+                        full_name AS fullName, 
+                        born_on as bornOn
+                    FROM users
+                  "
+        )->execute()
+            ->fetch(UserDTO::class);
+    }
+}
