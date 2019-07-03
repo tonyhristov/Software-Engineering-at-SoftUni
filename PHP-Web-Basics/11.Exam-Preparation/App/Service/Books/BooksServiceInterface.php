c03362ba2f4de8c5032e506cc4a0720edfb2f4e2
Antonio <47687247+tonyhristov@users.noreply.github.com>
2019-07-03 21:16:37 +0300

    Exercise

---
+++ PHP-Web-Basics/11.Exam-Preparation/App/Service/Books/BooksServiceInterface.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App\Service\Books;
+
+
+use App\Data\BookDTO;
+
+
+interface BooksServiceInterface
+{
+    public function add(BookDTO $bookDTO): bool;
+
+    public function edit(BookDTO $bookDTO): bool;
+
+    public function delete(int $id): bool;
+
+    /**
+     * @return \Generator | BookDTO[]
+     */
+    public function getAll(): \Generator;
+
+    public function getOneById(int $id): BookDTO;
+
+    public function getAllByAuthor();
+}
