ecdffb9b62091f42182f57110da11c3b358a2b0b
Antonio <47687247+tonyhristov@users.noreply.github.com>
2019-07-05 23:32:04 +0300

    Exercise

--- PHP-Web-Basics/11.Exam-Preparation/App/Template/books/editBook.php
+++ PHP-Web-Basics/11.Exam-Preparation/App/Template/books/editBook.php
@@ -1,38 +1,28 @@
-<?php
-/**
- * @var \App\Data\EditBookDTO $data
- */
-?>
+<?php /** @var \App\Data\EditBookDTO $data */ ?>
+<?php /** @var array $errors |null */ ?>
 
 <h1>EDIT BOOK</h1>
 
-<a href="profil.php">My Profile</a>
-<br>
+<a href="profile.php">My Profile</a><br/><br/>
+
+<?php foreach ($errors as $error): ?>
+    <p style="color: red"><?= $error ?></p>
+<?php endforeach; ?>
 
 <form method="post">
-    Book Title: <label>
-        <input type="text" name="title" value="<?= $data->getBook()->getTitle(); ?>"/>
-    </label> <br/>
-    Book Author: <label>
-        <input type="text" name="author" value="<?= $data->getBook()->getAuthor(); ?>"/>
-    </label><br/>
-    Description: <label>
-        <textarea rows="5" name="description"><?= $data->getBook()->getDescription();?></textarea>
-    </label><br/>
-    Image URL: <label>
-        <input type="text" name="image_url"/>
-    </label><br/>
-    Genre: <label>
-        <select name="genre_id"><br/>
-            <?php foreach ($data->getGenres() as $genre): ?>
-                <?php if ($data->getBook()->getGenre()->getId() === $genre->getId()): ?>
-                    <option selected="selected" value="<?= $genre->getId(); ?>"><?= $genre->getName(); ?></option>
-                <?php else: ?>
-                    <option value="<?= $genre->getId(); ?>"><?= $genre->getName(); ?></option>
-                <?php endif; ?>
-            <?php endforeach; ?>
-        </select>
-    </label><br/>
-    <img src="<?= $data->getBook()->getImageURL(); ?>" alt="None" width="200" height="100" /><br />
+    Book Title: <input type="text" name="title" value="<?= $data->getBook()->getTitle(); ?>"/> <br/>
+    Book Author: <input type="text" name="author" value="<?= $data->getBook()->getAuthor(); ?>"/><br/>
+    Description: <textarea rows="5" name="description"><?= $data->getBook()->getDescription(); ?></textarea><br/>
+    Image URL: <input type="text" name="image_url" value="<?= $data->getBook()->getImageURL(); ?>"/><br/>
+    Genre: <select name="genre_id"><br/>
+        <?php foreach ($data->getGenres() as $genre): ?>
+            <?php if ($data->getBook()->getGenre()->getId() === $genre->getId()): ?>
+                <option selected="selected" value="<?= $genre->getId(); ?>"><?= $genre->getName(); ?></option>
+            <?php else: ?>
+                <option value="<?= $genre->getId(); ?>"><?= $genre->getName(); ?></option>
+            <?php endif; ?>
+        <?php endforeach; ?>
+    </select><br/>
+    <img src="<?= $data->getBook()->getImageURL(); ?>" alt="None" width="200" height="100"/><br/>
     <input type="submit" value="Edit" name="edit"/><br/>
 </form>
