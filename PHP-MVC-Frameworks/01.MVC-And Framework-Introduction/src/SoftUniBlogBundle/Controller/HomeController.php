66d6926f836110427f5f2a5b5e3f54c45ecc50eb
Antonio <47687247+tonyhristov@users.noreply.github.com>
2019-07-17 00:11:54 +0300

    Exercise

---
+++ PHP-MVC-Frameworks/01.MVC-And Framework-Introduction/src/SoftUniBlogBundle/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace SoftUniBlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends Controller
+{
+    /**
+     * @Route("/", name="blog_index")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('home/index.html.twig');
+    }
+}
