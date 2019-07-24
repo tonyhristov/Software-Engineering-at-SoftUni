b60f3a1d648a5ffc9e8a81cf3d596ed754c369c2
Antonio <47687247+tonyhristov@users.noreply.github.com>
2019-07-24 14:39:20 +0300

    Exercise

--- PHP-MVC-Frameworks/Blog_Introduction_to_Symfony/src/SoftUniBlogBundle/Form/UserType.php
+++ PHP-MVC-Frameworks/Blog_Introduction_to_Symfony/src/SoftUniBlogBundle/Form/UserType.php
@@ -2,9 +2,7 @@
 
 namespace SoftUniBlogBundle\Form;
 
-use Symfony\Component\DependencyInjection\Compiler\RepeatedPass;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
