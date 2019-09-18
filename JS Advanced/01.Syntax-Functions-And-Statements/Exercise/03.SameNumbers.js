0e36582379d6399ce1e76850828d2d0d2b60189c
Antonio <47687247+tonyhristov@users.noreply.github.com>
2019-09-18 19:55:04 +0300

    Exercise

---
+++ JS Advanced/01.Syntax-Functions-And-Statements/Exercise/03.SameNumbers.js
@@ -0,0 +1,17 @@
+function solve(num) {
+    let numbersArr = String(num).split("");
+
+    let isSame = true;
+    let firstDigit = numbersArr[0];
+    for (let i = firstDigit; i < numbersArr.length; i++) {
+        let digitsAfterTheFirstOne = numbersArr[i];
+        if (firstDigit !== digitsAfterTheFirstOne) {
+            isSame = false;
+        }
+    }
+
+    let sumNumbers = numbersArr.map(Number).reduce((acc, cur) => acc + cur);
+
+    console.log(isSame);
+    console.log(sumNumbers);
+}
