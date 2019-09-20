04f3033fa2a6e41e97e0b094db034aac3d7660d0
Antonio <47687247+tonyhristov@users.noreply.github.com>
2019-09-20 20:56:25 +0300

    Exercise

--- JS Advanced/01.Syntax-Functions-And-Statements/Exercise/06.RoadRadar.js
+++ JS Advanced/01.Syntax-Functions-And-Statements/Exercise/06.RoadRadar.js
@@ -10,31 +10,29 @@ function solve(array) {
       } else if (speed >= 170) {
          console.log('reckless driving ');
       }
-   }else if(area === "interstate"){
-    if (speed > 90 && speed <= 110) {
-        console.log('speeding');
-     } else if (speed > 90 && speed <= 130) {
-        console.log('excessive speeding ');
-     } else if (speed >= 130) {
-        console.log('reckless driving ');
-     }
-   }else if(area === "city"){
-    if (speed > 50 && speed <= 70) {
-        console.log('speeding');
-     } else if (speed > 50 && speed <= 90) {
-        console.log('excessive speeding ');
-     } else if (speed >= 90) {
-        console.log('reckless driving ');
-     }
-   }else{
-    if (speed > 20 && speed <= 40) {
-        console.log('speeding');
-     } else if (speed > 60 && speed <= 60) {
-        console.log('excessive speeding ');
-     } else if (speed >= 60) {
-        console.log('reckless driving ');
-     }
+   } else if (area === 'interstate') {
+      if (speed > 90 && speed <= 110) {
+         console.log('speeding');
+      } else if (speed > 90 && speed <= 130) {
+         console.log('excessive speeding ');
+      } else if (speed >= 130) {
+         console.log('reckless driving ');
+      }
+   } else if (area === 'city') {
+      if (speed > 50 && speed <= 70) {
+         console.log('speeding');
+      } else if (speed > 50 && speed <= 90) {
+         console.log('excessive speeding ');
+      } else if (speed >= 90) {
+         console.log('reckless driving ');
+      }
+   } else {
+      if (speed > 20 && speed <= 40) {
+         console.log('speeding');
+      } else if (speed > 60 && speed <= 60) {
+         console.log('excessive speeding ');
+      } else if (speed >= 60) {
+         console.log('reckless driving ');
+      }
    }
 }
-
-solve([21, 'residential'])
