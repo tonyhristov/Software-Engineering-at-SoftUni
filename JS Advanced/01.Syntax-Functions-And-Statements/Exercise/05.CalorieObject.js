c53ea07359118a1d73c6bbd2b2bc93d0d0edb984
Antonio <47687247+tonyhristov@users.noreply.github.com>
2019-09-20 20:40:17 +0300

    Exercise

--- JS Advanced/01.Syntax-Functions-And-Statements/Exercise/05.CalorieObject.js
+++ JS Advanced/01.Syntax-Functions-And-Statements/Exercise/05.CalorieObject.js
@@ -1,12 +1,12 @@
 function solve(array) {
-    let obj = {};
+   let obj = {};
 
-    for (let i = 0; i < array.length; i += 2) {
-        let element = array[i];
-        let value = Number(array[i + 1]);
+   for (let i = 0; i < array.length; i += 2) {
+      let element = array[i];
+      let value = Number(array[i + 1]);
 
-        obj[element] = value;
-    }
+      obj[element] = value;
+   }
 
-    console.log(obj);
+   console.log(obj);
 }
