5823cbec697e3d865973ef285acb46dfa0855151
Antonio <47687247+tonyhristov@users.noreply.github.com>
2019-11-17 23:59:25 +0200

    Exercise

---
+++ JS Applications/05.Asynchronous-Programming/02. FISHER-GAME/fetch-service.js
@@ -0,0 +1,35 @@
+const catches = (function() {
+   const url = `https://fisher-game.firebaseio.com/catches/`;
+   function mkUrl(id) {
+      return `${url}${id}.json`;
+   }
+
+   const get = data => {
+      return fetch(mkUrl('')).then(res => res.json());
+   };
+
+   const post = data => {
+      const headers = {
+         method: 'POST',
+         headers: { 'Content-Type': 'application/json' },
+         body: JSON.stringify(data),
+      };
+
+      return fetch(mkUrl(''), headers).then(res => res.json());
+   };
+
+   const put = data => {};
+
+   const del = id => {
+      const headers = { method: 'DELETE' };
+
+      return fetch(mkUrl(id), headers).then(res => res.json());
+   };
+
+   return {
+      get,
+      post,
+      put,
+      del,
+   };
+})();
