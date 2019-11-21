6e0ebc03d2f28d82f802994f629751c26ebfdb3c
Antonio <47687247+tonyhristov@users.noreply.github.com>
2019-11-21 16:28:56 +0200

    Exercise

---
+++ JS Applications/06.Remote-Databases/01.BOOKS/html-elements.js
@@ -0,0 +1,10 @@
+export const html = {
+   $getAllBooks: () => document.getElementById('all-books'),
+   $getBookTitle: () => document.getElementById('title'),
+   $getBookAuthor: () => document.getElementById('author'),
+   $getBookIsbn: () => document.getElementById('isbn'),
+   $getEditTitle: () => document.getElementById('edit-title'),
+   $getEditAuthor: () => document.getElementById('edit-author'),
+   $getEditIsbn: () => document.getElementById('edit-isbn'),
+   $getEditId: () => document.getElementById('edit-id'),
+};
