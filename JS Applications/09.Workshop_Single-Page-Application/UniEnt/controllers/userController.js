4b409770caa91b469d6bda2a4a08d5b6da3417ff
Antonio <47687247+tonyhristov@users.noreply.github.com>
2019-12-03 22:42:13 +0200

    Exercise

---
+++ JS Applications/09.Workshop_Single-Page-Application/UniEnt/controllers/userController.js
@@ -0,0 +1,42 @@
+import { getTemplate, saveAndRedirect } from '../helpers/helper.js';
+import { register, logout, login } from '../models/userModel.js';
+import { removeUser } from '../helpers/storage.js';
+
+export function getLogin(context) {
+   getTemplate('users/login.hbs', context);
+}
+
+export function getRegister(context) {
+   getTemplate('users/register.hbs', context);
+}
+
+export function postRegister(context) {
+   if (context.params.password !== context.params.rePassword) {
+      alert('Passwords does not match!');
+      throw new Error('Passwords does not match!');
+   }
+
+   console.log(context);
+
+   let data = {
+      username: context.params.username,
+      password: context.params.password,
+   };
+
+   register(data)
+      .then(saveAndRedirect.bind(undefined, context, '#/home'))
+      .catch(console.log);
+}
+
+export function logoutUser(context) {
+   logout().then(function() {
+      removeUser();
+      context.redirect('#/home');
+   });
+}
+
+export function postLogin(context) {
+   login(context.params)
+      .then(saveAndRedirect.bind(undefined, context, '#/home'))
+      .catch(console.log);
+}
