98433c795dcc10e23bf323ee94cc61e4fa86ab45
Antonio <47687247+tonyhristov@users.noreply.github.com>
2019-12-03 22:42:09 +0200

    Exercise

---
+++ JS Applications/09.Workshop_Single-Page-Application/UniEnt/controllers/homeController.js
@@ -0,0 +1,11 @@
+import { getTemplate, checkContext } from '../helpers/helper.js';
+import { getAllEvents } from '../models/eventModel.js';
+
+export async function getHome(context) {
+   let newContext = checkContext(context);
+   let events = await getAllEvents();
+
+   newContext.events = events;
+
+   getTemplate('home.hbs', newContext);
+}
