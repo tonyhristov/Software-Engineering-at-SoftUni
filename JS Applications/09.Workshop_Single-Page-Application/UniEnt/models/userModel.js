21633f88f379c2517c9eb420d274ec48ef6bf45e
Antonio <47687247+tonyhristov@users.noreply.github.com>
2019-12-05 17:53:36 +0200

    Exercise

---
+++ JS Applications/09.Workshop_Single-Page-Application/UniEnt/models/userModel.js
@@ -0,0 +1,14 @@
+import { post } from '../helpers/requester.js';
+import { appKey } from '../helpers/storage.js';
+
+export function register(data) {
+   return post(`user/${appKey}`, data);
+}
+
+export function logout() {
+   return post(`user/${appKey}/_logout`);
+}
+
+export function login(data) {
+   return post(`user/${appKey}/login`, data);
+}
