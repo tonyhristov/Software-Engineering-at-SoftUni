573c5bab027b1cc188653d038ad6482e2ec7f535
Antonio <47687247+tonyhristov@users.noreply.github.com>
2019-12-20 10:39:51 +0200

    Exercise

---
+++ JS Applications/10.Exam-Preparation/The-Trekking-Zone/App/scripts/requester.js
@@ -0,0 +1,71 @@
+const baseUrl = 'https://baas.kinvey.com/';
+const appKey = 'kid_rJXcM0_pS';
+const appSecret = '38a11e30975e415697e2168eea9c1c63';
+
+function createAuthorization(type) {
+   return type === 'Basic'
+      ? `Basic ${btoa(`${appKey}:${appSecret}`)}`
+      : `Kinvey ${sessionStorage.getItem('authtoken')}`;
+}
+
+function createHeader(type, httpMethod, data) {
+   const headers = {
+      method: httpMethod,
+      headers: {
+         Authorization: createAuthorization(type),
+         'Content-Type': 'application/json',
+      },
+   };
+
+   if (httpMethod === 'POST' || httpMethod === 'PUT') {
+      headers.body = JSON.stringify(data);
+   }
+
+   return headers;
+}
+
+function handleError(e) {
+   if (!e.ok) {
+      throw new Error(e.statusText);
+   }
+   return e;
+}
+
+function deserializeData(x) {
+   if (x.status === 204) {
+      return x;
+   }
+   return x.json();
+}
+
+function fetchData(kinveyModule, endpoint, headers) {
+   const url = `${baseUrl}${kinveyModule}/${appKey}/${endpoint}`;
+
+   return fetch(url, headers)
+      .then(handleError)
+      .then(deserializeData);
+}
+
+export function get(kinveyModule, endpoint, type) {
+   const header = createHeader(type, 'GET');
+
+   return fetchData(kinveyModule, endpoint, header);
+}
+
+export function post(kinveyModule, endpoint, data, type) {
+   const header = createHeader(type, 'POST', data);
+
+   return fetchData(kinveyModule, endpoint, header);
+}
+
+export function put(kinveyModule, endpoint, data, type) {
+   const header = createHeader(type, 'PUT', data);
+
+   return fetchData(kinveyModule, endpoint, header);
+}
+
+export function del(kinveyModule, endpoint, type) {
+   const header = createHeader(type, 'DELETE');
+
+   return fetchData(kinveyModule, endpoint, header);
+}
