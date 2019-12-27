ee3d60e8ee4e8afbbb5438ca49299f4960087ad4
Antonio <47687247+tonyhristov@users.noreply.github.com>
2019-12-27 19:19:41 +0200

    Exercise

---
+++ JS Applications/10.Exam-Preparation/The-Trekking-Zone/App/scripts/shared.js
@@ -0,0 +1,11 @@
+export function getPartials() {
+   return {
+      header: './views/common/header.hbs',
+      footer: './views/common/footer.hbs',
+   };
+}
+
+export function setHeaderInfo(ctx) {
+   ctx.isAuth = sessionStorage.getItem('authtoken') !== null;
+   ctx.username = sessionStorage.getItem('username');
+}
