66a5da38a6cf23ca6b4a94e32310f5e669e82c7e
Antonio <47687247+tonyhristov@users.noreply.github.com>
2019-12-28 19:48:45 +0200

    Exercise

---
+++ JS Applications/10.Exam-Preparation/The-Trekking-Zone/App/scripts/handlers/homeHandler.js
@@ -0,0 +1,15 @@
+import { get } from '../requester.js';
+import { getPartials, setHeaderInfo } from '../shared.js';
+
+export function getHome(ctx) {
+   setHeaderInfo(ctx);
+
+   if (ctx.isAuth) {
+      get('appdata', 'treks', 'Kinvey').then(treks => {
+         ctx.treks = treks;
+         this.loadPartials(getPartials()).partial('./views/home.hbs');
+      });
+   } else {
+      this.loadPartials(getPartials()).partial('./views/home.hbs');
+   }
+}
