b5c9e789ead96d816bb55e37d5a235c6deb95fff
Antonio <47687247+tonyhristov@users.noreply.github.com>
2019-12-28 19:48:50 +0200

    Exercise

---
+++ JS Applications/10.Exam-Preparation/The-Trekking-Zone/App/scripts/handlers/trekHandler.js
@@ -0,0 +1,110 @@
+import { get, post, put, del } from '../requester.js';
+import { getPartials, setHeaderInfo } from '../shared.js';
+
+// Create
+export function getCreate(ctx) {
+   this.loadPartials(getPartials()).partial('./views/trek/create.hbs');
+}
+
+export function postCreate(ctx) {
+   const { location, dateTime, description, imageURL } = ctx.params;
+
+   const trek = {
+      location,
+      dateTime,
+      description,
+      imageURL,
+      organizer: sessionStorage.getItem('username'),
+      likesCounter: 0,
+   };
+   if (location && dateTime && description && imageURL) {
+      post('appdata', 'treks', trek, 'Kinvey')
+         .then(() => {
+            ctx.redirect('/');
+         })
+         .catch(console.error);
+   }
+}
+
+// Details
+export function getDetails(ctx) {
+   setHeaderInfo(ctx);
+   const id = ctx.params.id;
+   get('appdata', `treks/${id}`, 'Kinvey')
+      .then(trek => {
+         trek.isCreator =
+            sessionStorage.getItem('userId') === trek._acl.creator;
+         ctx.trek = trek;
+
+         this.loadPartials(getPartials()).partial('../views/trek/details.hbs');
+      })
+      .catch(console.error());
+}
+
+// Edit
+export function getEdit(ctx) {
+   setHeaderInfo(ctx);
+   const id = ctx.params.id;
+
+   get('appdata', `treks/${id}`, 'Kinvey')
+      .then(trek => {
+         ctx.trek = trek;
+
+         this.loadPartials(getPartials()).partial('../views/trek/edit.hbs');
+      })
+      .catch(console.error());
+}
+
+export function postEdit(ctx) {
+   const id = ctx.params.id;
+   const {
+      location,
+      dateTime,
+      description,
+      imageURL,
+      organizer,
+      likesCounter,
+   } = ctx.params;
+
+   const trek = {
+      location,
+      dateTime,
+      description,
+      imageURL,
+      organizer,
+      likesCounter,
+   };
+   if (location && dateTime && description && imageURL) {
+      put('appdata', `treks/${id}`, trek, 'Kinvey')
+         .then(() => {
+            ctx.redirect('/');
+         })
+         .catch(console.error);
+   }
+}
+
+// Delete
+export function getDelete(ctx) {
+   const id = ctx.params.id;
+
+   del('appdata', `treks/${id}`, 'Kinvey')
+      .then(() => {
+         ctx.redirect('/');
+      })
+      .catch(console.error());
+}
+
+// Like
+export function getLike(ctx) {
+   const id = ctx.params.id;
+
+   get('appdata', `treks/${id}`, 'Kinvey')
+      .then(trek => {
+         trek.likesCounter++;
+         return put('appdata', `treks/${id}`, trek, 'Kinvey');
+      })
+      .then(() => {
+         ctx.redirect(`/trek/${id}`);
+      })
+      .catch(console.error());
+}
