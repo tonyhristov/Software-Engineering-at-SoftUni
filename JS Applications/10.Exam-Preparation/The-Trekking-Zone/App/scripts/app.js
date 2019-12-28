ba1b268ba31b3af2dfc66ca045057ece4aceb773
Antonio <47687247+tonyhristov@users.noreply.github.com>
2019-12-28 19:48:33 +0200

    Exercise

--- JS Applications/10.Exam-Preparation/The-Trekking-Zone/App/scripts/app.js
+++ JS Applications/10.Exam-Preparation/The-Trekking-Zone/App/scripts/app.js
@@ -1,24 +1,13 @@
-import { get, post, put, del } from './requester.js';
-import { getPartials, setHeaderInfo } from './shared.js';
 import * as authHandler from './handlers/authHandler.js';
+import * as trekHandler from './handlers/trekHandler.js';
+import * as homeHandler from './handlers/homeHandler.js';
 
 (() => {
    const app = Sammy('body', function() {
       this.use('Handlebars', 'hbs');
 
       //Home
-      this.get('/', function(ctx) {
-         setHeaderInfo(ctx);
-
-         if (ctx.isAuth) {
-            get('appdata', 'treks', 'Kinvey').then(treks => {
-               ctx.treks = treks;
-               this.loadPartials(getPartials()).partial('./views/home.hbs');
-            });
-         } else {
-            this.loadPartials(getPartials()).partial('./views/home.hbs');
-         }
-      });
+      this.get('/', homeHandler.getHome);
 
       //USER
 
@@ -39,114 +28,21 @@ import * as authHandler from './handlers/authHandler.js';
       //TREKS
 
       //Trek => Create
-      this.get('/create', function(ctx) {
-         this.loadPartials(getPartials()).partial('./views/trek/create.hbs');
-      });
-      this.post('/create', function(ctx) {
-         const { location, dateTime, description, imageURL } = ctx.params;
-
-         const trek = {
-            location,
-            dateTime,
-            description,
-            imageURL,
-            organizer: sessionStorage.getItem('username'),
-            likesCounter: 0,
-         };
-         if (location && dateTime && description && imageURL) {
-            post('appdata', 'treks', trek, 'Kinvey')
-               .then(() => {
-                  ctx.redirect('/');
-               })
-               .catch(console.error);
-         }
-      });
+      this.get('/create', trekHandler.getCreate);
+      this.post('/create', trekHandler.postCreate);
 
       //Trek => Details
-      this.get('/trek/:id', function(ctx) {
-         setHeaderInfo(ctx);
-         const id = ctx.params.id;
-         get('appdata', `treks/${id}`, 'Kinvey')
-            .then(trek => {
-               trek.isCreator =
-                  sessionStorage.getItem('userId') === trek._acl.creator;
-               ctx.trek = trek;
-
-               this.loadPartials(getPartials()).partial(
-                  '../views/trek/details.hbs'
-               );
-            })
-            .catch(console.error());
-      });
+      this.get('/trek/:id', trekHandler.getDetails);
 
       //Trek => Edit
-      this.get('/edit/:id', function(ctx) {
-         setHeaderInfo(ctx);
-         const id = ctx.params.id;
-
-         get('appdata', `treks/${id}`, 'Kinvey')
-            .then(trek => {
-               ctx.trek = trek;
-
-               this.loadPartials(getPartials()).partial(
-                  '../views/trek/edit.hbs'
-               );
-            })
-            .catch(console.error());
-      });
-      this.post('/edit/:id', function(ctx) {
-         const id = ctx.params.id;
-         const {
-            location,
-            dateTime,
-            description,
-            imageURL,
-            organizer,
-            likesCounter,
-         } = ctx.params;
-
-         const trek = {
-            location,
-            dateTime,
-            description,
-            imageURL,
-            organizer,
-            likesCounter,
-         };
-         if (location && dateTime && description && imageURL) {
-            put('appdata', `treks/${id}`, trek, 'Kinvey')
-               .then(() => {
-                  ctx.redirect('/');
-               })
-               .catch(console.error);
-         }
-      });
+      this.get('/edit/:id', trekHandler.getEdit);
+      this.post('/edit/:id', trekHandler.postEdit);
 
       //Trek => Delete
-      this.get('/close/:id', function(ctx) {
-         const id = ctx.params.id;
-
-         del('appdata', `treks/${id}`, 'Kinvey')
-            .then(() => {
-               ctx.redirect('/');
-            })
-            .catch(console.error());
-      });
+      this.get('/close/:id', trekHandler.getDelete);
 
       //Trek => Like
-      this.get('/like/:id', function(ctx) {
-         const id = ctx.params.id;
-
-         get('appdata', `treks/${id}`, 'Kinvey')
-            .then(trek => {
-               trek.likesCounter++;
-               return put('appdata', `treks/${id}`, trek, 'Kinvey');
-            })
-            .then(() => {
-               ctx.redirect(`/trek/${id}`);
-            })
-            .catch(console.error());
-      });
+      this.get('/like/:id', trekHandler.getLike);
    });
 
    app.run('/');
