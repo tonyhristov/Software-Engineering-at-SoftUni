5faf42de7ae82d892e8a72a489daac31a5371d26
Antonio <47687247+tonyhristov@users.noreply.github.com>
2020-04-17 00:01:43 +0300

    Exercise

--- JS Applications/10.Exam-Preparation/CookUni_Resources/scripts/shared.js
+++ JS Applications/10.Exam-Preparation/CookUni_Resources/scripts/shared.js
@@ -9,3 +9,32 @@ export function setHeaderInfo(ctx) {
    ctx.isAuth = sessionStorage.getItem('authtoken') !== null;
    ctx.fullName = sessionStorage.getItem('fullName');
 }
+
+export function displayError(message) {
+   const errorBox = document.getElementById(`errorBox`);
+
+   errorBox.style.display = 'block';
+   errorBox.textContent = message;
+   setTimeout(() => {
+      errorBox.style.display = 'none';
+   }, 3000);
+}
+
+export function displaySuccess(message) {
+   const successBox = document.getElementById(`successBox`);
+
+   successBox.style.display = 'block';
+   successBox.textContent = message;
+   setTimeout(() => {
+      successBox.style.display = 'none';
+   }, 3000);
+}
+
+export function displayLoading() {
+   const loadingBox = document.getElementById(`loadingBox`);
+
+   loadingBox.style.display = 'block';
+   setTimeout(() => {
+      loadingBox.style.display = 'none';
+   }, 3000);
+}
