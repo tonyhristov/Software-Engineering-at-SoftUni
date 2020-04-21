7b84487302fcb64ecbd56c7457e12ff9dbf41dfc
Antonio <47687247+tonyhristov@users.noreply.github.com>
2020-04-21 13:12:22 +0300

    Exerrcise

--- JS Applications/10.Exam-Preparation/The-Trekking-Zone/App/scripts/shared.js
+++ JS Applications/10.Exam-Preparation/The-Trekking-Zone/App/scripts/shared.js
@@ -9,3 +9,51 @@ export function setHeaderInfo(ctx) {
    ctx.isAuth = sessionStorage.getItem('authtoken') !== null;
    ctx.username = sessionStorage.getItem('username');
 }
+
+function fadeAway(box) {
+   const fadeEffect = setInterval(() => {
+      if (!box.style.opacity) {
+         box.style.opacity = 1;
+      }
+
+      if (box.style.opacity > 0) {
+         box.style.opacity -= 0.1;
+      } else {
+         clearInterval(fadeEffect);
+         box.style.opacity += 1;
+         box.style.display = 'none';
+      }
+   }, 40);
+}
+
+export function displaySuccess(message) {
+   const successBox = document.getElementById('successBox');
+
+   successBox.style.display = 'block';
+   successBox.textContent = message;
+
+   setTimeout(() => {
+      fadeAway(successBox);
+   }, 2000);
+}
+
+export function displayLoading() {
+   const loadingBox = document.getElementById('loadingBox');
+
+   loadingBox.style.display = 'block';
+
+   setTimeout(() => {
+      fadeAway(loadingBox);
+   }, 2000);
+}
+
+export function displayError(message) {
+   const errorBox = document.getElementById('errorBox');
+
+   errorBox.style.display = 'block';
+   errorBox.textContent = message;
+
+   setTimeout(() => {
+      fadeAway(errorBox);
+   }, 2000);
+}
