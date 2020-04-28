990e4e8431267b5403e9dca4082a643ac3dab169
Antonio <47687247+tonyhristov@users.noreply.github.com>
2020-04-28 12:33:39 +0300

    Exercise

--- JS Applications/10.Exam-Preparation/The-Trekking-Zone/App/scripts/handlers/homeHandler.js
+++ JS Applications/10.Exam-Preparation/The-Trekking-Zone/App/scripts/handlers/homeHandler.js
@@ -1,12 +1,13 @@
 import { get } from '../requester.js';
-import { getPartials, setHeaderInfo } from '../shared.js';
+import { getPartials, setHeaderInfo, displayLoading } from '../shared.js';
 
 export function getHome(ctx) {
    setHeaderInfo(ctx);
 
    if (ctx.isAuth) {
-      get('appdata', 'treks', 'Kinvey').then(treks => {
+      get('appdata', 'treks', 'Kinvey').then((treks) => {
          ctx.treks = treks;
+
          this.loadPartials(getPartials()).partial('./views/home.hbs');
       });
    } else {
