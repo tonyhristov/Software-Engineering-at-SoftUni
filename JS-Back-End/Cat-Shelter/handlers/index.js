8c9d2fd02129a2da47ad524784179ed1cbf31902
Antonio <47687247+tonyhristov@users.noreply.github.com>
2020-05-22 18:47:46 +0300

    Practicing NodeJS

---
+++ JS-Back-End/Cat-Shelter/handlers/index.js
@@ -0,0 +1,5 @@
+const homeHandler = require('./homeHandler');
+const staticHandler = require('./staticHandler');
+const catHandler = require('./catHandler');
+
+module.exports = [homeHandler, staticHandler, catHandler];
