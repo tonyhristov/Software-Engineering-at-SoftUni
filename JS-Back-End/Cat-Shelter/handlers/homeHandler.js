a6b5930cfcd828d234faf94a21dc6e2837ae44e6
Antonio <47687247+tonyhristov@users.noreply.github.com>
2020-05-26 19:03:45 +0300

    Update homeHandler.js

--- JS-Back-End/Cat-Shelter/handlers/homeHandler.js
+++ JS-Back-End/Cat-Shelter/handlers/homeHandler.js
@@ -1,34 +1,50 @@
 const url = require('url');
 const fs = require('fs');
 const path = require('path');
-// const cats = require('../data/cats.json');
+const cats = require('../data/cats');
 
 module.exports = (req, res) => {
    const pathname = url.parse(req.url).pathname;
 
    if (pathname === '/' && req.method === 'GET') {
-      const filePath = path.normalize(
-         path.join(__dirname, '../views/home/index.html')
-      );
+      const index = fs.createReadStream(`./views/home/index.html`);
 
-      fs.readFile(filePath, (err, data) => {
-         if (err) {
-            console.log(err);
-            res.writeHead(404, {
-               'Content-Type': 'text/plain',
-            });
+      index.on('data', (data) => {
+         const catPlaceholder = cats.map(
+            (cat) =>
+               `<li>
+                  <img
+                     src="${path.join('./content/post-images/' + cat.image)}"
+                     alt="${cat.name}"
+                  />
+                  <h3>${cat.name}</h3>
+                  <p><span>Breed: </span>${cat.breed}</p>
+                  <p>
+                     <span>Description: </span>${cat.description}
+                  </p>
+                  <ul class="buttons">
+                     <li class="btn edit"><a href="/cats-edit/${
+                        cat.id
+                     }">Change Info</a></li>
+                     <li class="btn delete"><a href="/cats-find-new-home/${
+                        cat.id
+                     }">New Home</a></li>
+                  </ul>
+               </li>`
+         );
+         const modifiedData = data
+            .toString()
+            .replace('{{cats}}', catPlaceholder);
 
-            res.write('404 Not Found');
-            res.end();
-            return;
-         }
+         res.write(modifiedData);
+      });
 
-         res.writeHead(200, {
-            'Content-Type': 'text/html',
-         });
-
-         res.write(data);
+      index.on('end', () => {
          res.end();
+      });
+
+      index.on('error', (err) => {
+         console.log(err);
       });
    } else {
       return true;
