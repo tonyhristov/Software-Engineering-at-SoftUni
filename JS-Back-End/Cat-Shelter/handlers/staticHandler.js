07cd90a9831acb3357a1fb35819d6d93fe03d061
Antonio <47687247+tonyhristov@users.noreply.github.com>
2020-05-26 19:04:05 +0300

    Update staticHandler.js

--- JS-Back-End/Cat-Shelter/handlers/staticHandler.js
+++ JS-Back-End/Cat-Shelter/handlers/staticHandler.js
@@ -19,25 +19,37 @@ module.exports = (req, res) => {
    const pathname = url.parse(req.url).pathname;
 
    if (pathname.startsWith('/content') && req.method === 'GET') {
-      fs.readFile(`./${pathname}`, 'utf-8', (err, data) => {
-         if (err) {
-            console.log(err);
-
-            res.writeHead(404, {
-               'Content-Type': 'text/plain',
-            });
-
-            res.write('Error was found');
-            res.end();
-            return;
-         }
-
-         res.writeHead(200, { 'Content-Type': getContentType(pathname) });
-
-         res.write(data);
-         res.end();
-      });
+      if (
+         pathname.endsWith('png') ||
+         pathname.endsWith('jpeg') ||
+         pathname.endsWith('jpg') ||
+         (pathname.endsWith('ico') && req.method === 'GET')
+      ) {
+         showingFile(pathname, res);
+      } else {
+         showingFile(pathname, res, 'utf-8');
+      }
    } else {
       return true;
    }
 };
+
+function showingFile(pathname, res, encoding) {
+   fs.readFile(`./${pathname}`, encoding, (err, data) => {
+      if (err) {
+         console.log(err);
+
+         res.writeHead(404, {
+            'Content-Type': 'text/plain',
+         });
+
+         res.write('Error was found');
+         res.end();
+
+         return;
+      }
+      res.writeHead(200, { 'Content-Type': getContentType(pathname) });
+      res.write(data);
+      res.end();
+   });
+}
