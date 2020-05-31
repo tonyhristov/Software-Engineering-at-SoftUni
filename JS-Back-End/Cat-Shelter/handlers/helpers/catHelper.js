2a3b7a4189f16bef6193668c25ba3c2604507d58
Antonio <47687247+tonyhristov@users.noreply.github.com>
2020-05-31 19:46:03 +0300

    Update catHelper.js

--- JS-Back-End/Cat-Shelter/handlers/helpers/catHelper.js
+++ JS-Back-End/Cat-Shelter/handlers/helpers/catHelper.js
@@ -6,19 +6,25 @@ const formidable = require('formidable');
 const breeds = require('../../data/breeds.json');
 const cats = require('../../data/cats.json');
 
-function processingPostReq(req, res, pathname, fields, files) {
+function handlingPost(req, res, pathname, fields, files, page) {
    if (pathname === 'cats.json') {
-      const oldPath = files.upload.path;
-      const newPath = './content/post-Images/' + files.upload.name;
+      if (page === 'add') {
+         const oldPath = files.upload.path;
+         const newPath = './content/post-Images/' + files.upload.name;
 
-      fs.rename(oldPath, newPath, (err) => {
-         if (err) {
-            console.log(err);
-         }
-         console.log('Files uploaded successfully!');
-      });
+         fs.rename(oldPath, newPath, (err) => {
+            if (err) {
+               console.log(err);
+            }
+            console.log('Files uploaded successfully!');
+         });
 
-      savingReq(res, pathname, fields, files);
+         savingReq(res, pathname, fields, files);
+      } else if (page === 'edit') {
+         //TODO
+      } else {
+         //TODO
+      }
    } else if (pathname === 'breeds.json') {
       let formData = '';
 
@@ -88,8 +94,64 @@ function findPath(res, pathName) {
    });
 }
 
+function renderHtml(req, res, pathname) {
+   const index = fs.createReadStream(`./views/${pathname}`);
+   const currentCatId = req.url.split('/')[2];
+
+   index.on('data', (data) => {
+      cats.map((cat) => {
+         if (cat.id == currentCatId) {
+            let modifiedData = data.toString().replace('{{cat}}', currentCatId);
+
+            modifiedData = modifiedData.replace('{{name}}', cat.name);
+            modifiedData = modifiedData.replace(
+               '{{image}}',
+               path.join('../content/post-Images/' + cat.image)
+            );
+            modifiedData = modifiedData.replace(
+               '{{description}}',
+               cat.description
+            );
+
+            if (pathname === 'catShelter.html') {
+               modifiedData = modifiedData.replace('{{altName}}', cat.name);
+            }
+
+            const breedsOptions = breeds.map((breed) => {
+               let renderCount = 0;
+               let render = '';
+
+               if (breed === cat.breed && renderCount < 1) {
+                  render = `<option value="${cat.breed}" selected>${cat.breed}</option>`;
+                  renderCount++;
+               } else {
+                  render = `<option value="${breed}" >${breed}</option>`;
+               }
+
+               return render;
+            });
+            modifiedData = modifiedData.replace(
+               '{{catBreeds}}',
+               breedsOptions.join('/')
+            );
+
+            res.write(modifiedData);
+         }
+      });
+   });
+
+   index.on('end', () => {
+      res.end();
+   });
+
+   index.on('error', (err) => {
+      console.log(err);
+   });
+}
+
 module.exports = {
-   processingPostReq,
+   handlingPost,
    savingReq,
    findPath,
+   renderHtml,
 };
