e6cda592978f54040a87d1cb8db8d4e8f8d34a7b
Antonio <47687247+tonyhristov@users.noreply.github.com>
2020-06-20 20:41:15 +0300

    Rename accesory model

---
+++ JS-Back-End/Cubicle/models/accessory.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const AccessorySchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true, maxLength: 2000 },
+  imageUrl: { type: String, required: true },
+  cubes: [{ type: "ObjectId", ref: "Cube" }],
+});
+
+AccessorySchema.path("imageUrl").validate(function (url) {
+  return url.startsWith("http://") || url.startsWith("https://");
+}, "Image URL is not valid!");
+
+module.exports = mongoose.model("Accessory", AccessorySchema);
