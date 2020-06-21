c11f7a4d83f63314e2b7890be392f04076dca931
Antonio <47687247+tonyhristov@users.noreply.github.com>
2020-06-21 14:26:26 +0300

    Update cubes.js

--- JS-Back-End/Cubicle/controllers/cubes.js
+++ JS-Back-End/Cubicle/controllers/cubes.js
@@ -4,12 +4,29 @@ const getAllCubes = async () => {
   const cubes = await Cube.find().lean();
   return cubes;
 };
+
 const getOneCube = async (id) => {
   const cube = await Cube.findById(id).lean();
   return cube;
 };
 
+const getCubeWithAccessories = async (id) => {
+  const cube = await Cube.findById(id).populate("accessories").lean();
+
+  return cube;
+};
+
+const updateCube = async (cubeId, accessoryId) => {
+  await Cube.findByIdAndUpdate(cubeId, {
+    $addToSet: {
+      accessories: [accessoryId],
+    },
+  });
+};
+
 module.exports = {
   getAllCubes,
   getOneCube,
+  updateCube,
+  getCubeWithAccessories,
 };
