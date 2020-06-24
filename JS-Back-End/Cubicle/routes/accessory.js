59c771c74bcd4124d530ece81aa614a255a64daf
Antonio <47687247+tonyhristov@users.noreply.github.com>
2020-06-24 19:46:42 +0300

    Update accessory.js

--- JS-Back-End/Cubicle/routes/accessory.js
+++ JS-Back-End/Cubicle/routes/accessory.js
@@ -1,16 +1,20 @@
 const { Router } = require("express");
-const { getOneCube } = require("../controllers/cubes");
-const { updateCube } = require("../controllers/cubes");
-const { getAccessories } = require("../controllers/accessories");
 const Accessory = require("../models/accessory");
-
+const { getOneCube, updateCube } = require("../controllers/cubes");
+const { getAccessories } = require("../controllers/accessories");
+const { authAccess } = require("../controllers/user");
+const { authAccessJson } = require("../controllers/user");
+const { getUserStatus } = require("../controllers/user");
 const router = Router();
 
-router.get("/create/accessory", (req, res) => {
-  res.render("createAccessory", { title: "Create Accessory" });
+router.get("/create/accessory", authAccess, getUserStatus, (req, res) => {
+  res.render("createAccessory", {
+    title: "Create Accessory",
+    isLoggedIn: res.isLoggedIn,
+  });
 });
 
-router.post("/create/accessory", async (req, res) => {
+router.post("/create/accessory", authAccessJson, async (req, res) => {
   const { name, description, imageUrl } = req.body;
 
   const accessory = new Accessory({ name, description, imageUrl });
@@ -26,31 +30,37 @@ router.post("/create/accessory", async (req, res) => {
   res.redirect("/create/accessory");
 });
 
-router.get("/attach/accessory/:id", async (req, res) => {
-  const cube = await getOneCube(req.params.id);
-  const accessories = await getAccessories();
+router.get(
+  "/attach/accessory/:id",
+  authAccess,
+  getUserStatus,
+  async (req, res) => {
+    const cube = await getOneCube(req.params.id);
+    const accessories = await getAccessories();
 
-  const cubeAccessories = cube.accessories.map((acc) =>
-    acc._id.valueOf().toString()
-  );
+    const cubeAccessories = cube.accessories.map((acc) =>
+      acc._id.valueOf().toString()
+    );
 
-  const notAttachedAccessories = accessories.filter((acc) => {
-    const accessoriesString = acc._id.valueOf().toString();
-    return !cubeAccessories.includes(accessoriesString);
-  });
+    const notAttachedAccessories = accessories.filter((acc) => {
+      const accessoriesString = acc._id.valueOf().toString();
+      return !cubeAccessories.includes(accessoriesString);
+    });
 
-  const canAttachAccessory =
-    cube.accessories.length !== accessories.length && accessories.length > 0;
+    const canAttachAccessory =
+      cube.accessories.length !== accessories.length && accessories.length > 0;
 
-  res.render("attachAccessory", {
-    title: "Attach Accessory",
-    ...cube,
-    accessories: notAttachedAccessories,
-    canAttachAccessory,
-  });
-});
+    res.render("attachAccessory", {
+      title: "Attach Accessory",
+      ...cube,
+      accessories: notAttachedAccessories,
+      canAttachAccessory,
+      isLoggedIn: res.isLoggedIn,
+    });
+  }
+);
 
-router.post("/attach/accessory/:id", async (req, res) => {
+router.post("/attach/accessory/:id", authAccessJson, async (req, res) => {
   const { accessory } = req.body;
   await updateCube(req.params.id, accessory);
 
