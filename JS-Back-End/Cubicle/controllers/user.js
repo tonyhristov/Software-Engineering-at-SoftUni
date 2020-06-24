f864a329d9cb4e26779cc09838ae6a85e8268fd4
Antonio <47687247+tonyhristov@users.noreply.github.com>
2020-06-24 19:46:36 +0300

    Update user.js

--- JS-Back-End/Cubicle/controllers/user.js
+++ JS-Back-End/Cubicle/controllers/user.js
@@ -1,8 +1,15 @@
-const User = require("../models/user");
+const env = process.env.NODE_ENV || "development";
+
 const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+const config = require("../config/config")[env];
 const bcrypt = require("bcrypt");
 
-const privateKey = "CUBE-WORKSHOP-SOFTUNI";
+const generateToken = (data) => {
+  const token = jwt.sign(data, config.privateKey);
+
+  return token;
+};
 
 const saveUser = async (req, res) => {
   const { username, password } = req.body;
@@ -13,9 +20,9 @@ const saveUser = async (req, res) => {
 
   const userObject = await user.save();
 
-  const token = jwt.sign(
+  const token = generateToken(
     { userID: userObject._id, username: userObject.username },
-    privateKey
+    config.privateKey
   );
 
   res.cookie("aid", token);
@@ -23,4 +30,84 @@ const saveUser = async (req, res) => {
   return true;
 };
 
-module.exports = { saveUser };
+const verifyUser = async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await User.findOne({ username });
+
+  const status = await bcrypt.compare(password, user.password);
+
+  const token = generateToken(
+    { userID: user._id, username: user.username },
+    config.privateKey
+  );
+
+  res.cookie("aid", token);
+
+  return status;
+};
+
+const authAccess = (req, res, next) => {
+  const token = req.cookies["aid"];
+
+  if (!token) {
+    res.redirect("/");
+  }
+
+  try {
+    jwt.verify(token, config.privateKey);
+    next();
+  } catch (err) {
+    return res.redirect("/");
+  }
+};
+
+const authAccessJson = (req, res, next) => {
+  const token = req.cookies["aid"];
+
+  if (!token) {
+    return res.json({ error: "Not authenticated" });
+  }
+
+  try {
+    jwt.verify(token, config.privateKey);
+    next();
+  } catch (err) {
+    return res.json({ error: "Not authenticated" });
+  }
+};
+
+const guestAccess = (req, res, next) => {
+  const token = req.cookies["aid"];
+
+  if (token) {
+    return res.redirect("/");
+  }
+  next();
+};
+
+const getUserStatus = (req, res, next) => {
+  const token = req.cookies["aid"];
+
+  if (!token) {
+    res.isLoggedIn = false;
+  }
+
+  try {
+    jwt.verify(token, config.privateKey);
+    res.isLoggedIn = true;
+  } catch (err) {
+    res.isLoggedIn = false;
+  }
+
+  next();
+};
+
+module.exports = {
+  saveUser,
+  verifyUser,
+  authAccess,
+  guestAccess,
+  getUserStatus,
+  authAccessJson,
+};
