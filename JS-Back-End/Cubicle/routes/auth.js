a05f22f0c58c7868c3cf7f56edc252f065bc96bd
Antonio <47687247+tonyhristov@users.noreply.github.com>
2020-06-27 15:11:58 +0300

    Update auth.js

--- JS-Back-End/Cubicle/routes/auth.js
+++ JS-Back-End/Cubicle/routes/auth.js
@@ -17,15 +17,29 @@ router.post("/login", async (req, res) => {
 });
 
 router.get("/signup", guestAccess, getUserStatus, (req, res) => {
-  res.render("registerPage", { isLoggedIn: res.isLoggedIn });
+  const error = req.query.error ? "Username or password is not valid" : null;
+
+  res.render("registerPage", { isLoggedIn: res.isLoggedIn, error });
 });
 
 router.post("/signup", async (req, res) => {
-  const status = await saveUser(req, res);
+  const { password } = req.body;
 
-  if (status) {
-    res.redirect("/");
+  if (!password || password.length < 8 || !password.match(/^[A-Za-z0-9]+$/)) {
+    return res.render("registerPage", {
+      error: "Username or password is not valid",
+    });
   }
+
+  const { error } = await saveUser(req, res);
+
+  if (error) {
+    return res.render("registerPage", {
+      error: "Username or password is not valid",
+    });
+  }
+
+  res.redirect("/");
 });
 
 module.exports = router;
