d67553c6411c75a30885ee7ee8d726af92aba9f9
Antonio <47687247+tonyhristov@users.noreply.github.com>
2020-06-27 15:11:55 +0300

    Update user.js

--- JS-Back-End/Cubicle/models/user.js
+++ JS-Back-End/Cubicle/models/user.js
@@ -1,8 +1,17 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  password: { type: String, required: true },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/^[A-Za-z0-9]+$/, "Username is not valid"],
+    minLength: 5,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model("User", userSchema);
