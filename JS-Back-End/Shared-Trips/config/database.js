ce381c254f421a670fea87120ac6455edf7fd6b3
Antonio <47687247+tonyhristov@users.noreply.github.com>
2020-07-01 21:56:54 +0300

    Create database.js

---
+++ JS-Back-End/Shared-Trips/config/database.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const dbString = require("./config").dbUrl;
+const rdyString = `${"*".repeat(10)}Database is Ready${"*".repeat(10)}`;
+
+module.exports = () => {
+  return mongoose.connect(
+    dbString,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    },
+    console.log(rdyString)
+  );
+};
