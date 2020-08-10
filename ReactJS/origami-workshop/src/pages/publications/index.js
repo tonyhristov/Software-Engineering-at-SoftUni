c461e5d1bbfd3c8c5b3c14a4dfdd43e3369bcc70
Antonio <47687247+tonyhristov@users.noreply.github.com>
2020-08-10 23:28:38 +0300

    feat: add hooks

--- ReactJS/origami-workshop/src/pages/publications/index.js
+++ ReactJS/origami-workshop/src/pages/publications/index.js
@@ -1,24 +1,18 @@
-import React, { Component } from "react";
+import React, { Component, useContext } from "react";
 import PageLayout from "../../components/page-layout";
 import styles from "./index.module.css";
 import Title from "../../components/title";
 import Origamis from "../../components/origamis";
-import UserContext from "../../context";
 
-class Publications extends Component {
-  static contextType = UserContext;
-
-  render() {
-    console.log(this.context);
-    return (
-      <PageLayout>
-        <div className={styles.container}>
-          <Title title="Publications" />
-          <Origamis />
-        </div>
-      </PageLayout>
-    );
-  }
-}
+const Publications = () => {
+  return (
+    <PageLayout>
+      <div className={styles.container}>
+        <Title title="Publications" />
+        <Origamis />
+      </div>
+    </PageLayout>
+  );
+};
 
 export default Publications;
